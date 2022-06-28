9c8262211d4015edb6cf56d338b191946034a621
Dhruv Bhardwaj <96115027+Dhruv-bhardwaj99@users.noreply.github.com>
2022-06-29 03:30:34 +0530

    stop watch

---
+++ stop-watch/src/Components/StopWatch.jsx
@@ -0,0 +1,35 @@
+import { useEffect, useRef, useState } from "react";
+
+export const StopWatch = () => {
+  const [counter, setCounter] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    startTimer();
+  }, []);
+
+  const startTimer = () => {
+    ref.current = setInterval(() => {
+      setCounter((p) => p + 1);
+    }, 1000);
+  };
+
+  return (
+    <div>
+      <h3>Stop Watch:{counter}</h3>
+      <button
+        onClick={() => {
+          clearInterval(ref.current);
+          console.log("The watch has been Stopped:", ref.current);
+        }}
+      >
+        Stop
+      </button>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={() =>{
+          clearInterval(ref.current);
+          setCounter(0);
+      }}>Reset</button>
+    </div>
+  );
+};
