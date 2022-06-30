1f2427271697abba2bb7ad9ce0b48c60027b8fc4
Dhruv Bhardwaj <96115027+Dhruv-bhardwaj99@users.noreply.github.com>
2022-06-30 09:27:07 +0530

    final

--- be-u/src/Components/Search/Search.jsx
+++ be-u/src/Components/Search/Search.jsx
@@ -1,50 +1,67 @@
 import { useEffect } from "react";
-import { useState } from "react"
-import axios from "axios"
-import "./Search.css"
+import { useState } from "react";
+import axios from "axios";
+import "./Search.css";
 
-export const Search = () =>{
-    const [value, setValue] = useState([]);
-    const [store, setStore] = useState([]);
-    useEffect(() =>{
-        getData();
-    }, [])
+export const Search = () => {
+  const [bag, setBag] = useState([]);
+  const [store, setStore] = useState([]);
+  const [filter, setFilter] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    const getData = ()=>{
-        axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) =>{
-            console.log(res.data);
-            setValue(res.data);
-        })
-    };
+  const getData = () => {
+    axios.get(`https://jsonplaceholder.typicode.com/posts`).then((res) => {
+    //   console.log(res.data);
+      setBag(res.data);
+    });
+  };
 
-    const handleChange = () =>{
-        axios.get(`https://jsonplaceholder.typicode.com/posts/${store}`).then((res) =>{
-            setValue(res.data);
-            console.log(res.data);
-        });
-    }
+  const handleChange = (e) => {
+    // const searchWord = e.target.value;
+    const final = bag.filter((res) => {
+        console.log(res.title.toLowerCase().includes(store.toLowerCase()))
+        return res.title.toLowerCase().includes(store.toLowerCase());
+      });
+    setFilter(final);
+  };
 
-    const storedData = (e) =>{
-        setStore(e.target.value);
-    }
+  return (
+    <div>
+      <h1>BeU</h1>
+      <input
+        type="text"
+        placeholder="Search Here"
+        onChange={(e) => {
+          setStore(e.target.value);
+        }}
+      />
+      <button onClick={handleChange}>Filter</button>
 
-    return(
+      {setStore.length !== 0 ? (
         <div>
-            <h1>BeU</h1>
-            <input type="text" placeholder="Search Here" onChange={storedData} />
-            <button onClick={handleChange}>Filter</button>
-
-            <div>
-                <div className="titleName">
-                    {value.map((e) =>(
-                        <div key={e.id} className="card">
-                            <h1>{e.title}</h1>
-                            <h4>{e.body}</h4>
-                        </div>
-                    ))}
-                </div>
-            </div>
-
+          <div className="titleName">
+            {filter.map((e) => (
+              <div key={e.id} className="card">
+                <h1>{e.title}</h1>
+                <h4>{e.body}</h4>
+              </div>
+            ))}
+          </div>
         </div>
-    )
-}
+      ) : (
+        <div>
+          <div className="titleName">
+            {bag.map((e) => (
+              <div key={e.id} className="card">
+                <h1>{e.title}</h1>
+                <h4>{e.body}</h4>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
